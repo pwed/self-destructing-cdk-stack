7112a714cd16bd1a62ea7511100d9f2bd26f1472
Fred  Stoddart <fred.stoddart@ac3.com.au>
2022-06-03 17:06:03 +1000

    tidy imports

--- dummy_stack/dummy_stack_stack.py
+++ dummy_stack/dummy_stack_stack.py
@@ -3,14 +3,14 @@ from aws_cdk import (
     Stack,
 )
 from constructs import Construct
-import pwed_cdk
+from pwed_cdk import pwed_ttl
 
 class DummyStackStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        ttl = pwed_cdk.pwed_ttl.Ttl(self, "pwed_ttl",
+        ttl = pwed_ttl.Ttl(self, "pwed_ttl",
             ttl=Duration.minutes(5),
             poll_interval=Duration.minutes(1),
         )
